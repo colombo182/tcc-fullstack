19909925086dca032d2096475a3190cb7f9a4b57
colombo182 <pedro_colombo@hotmail.com>
2025-05-07 14:58:37 -0300

    TCC - Fullstack - v1.0.1

--- installers/tcc.sh
+++ installers/tcc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file is still here to keep PM2 working on older installations.
-
+# Add  "bash /home/colombo182/tcc-fullstack/installers/tcc.sh" inside ~~/.bashrc to autostart the application
 cd /home/colombo182/tcc-fullstack
 DISPLAY=:0 xrandr --output HDMI-1 --rotate left
 DISPLAY=:0 npm start
